ab6004f1461a68734efef29ba1a21ea5581abcc6
Roberts Treize <93783050+streboreziert@users.noreply.github.com>
2024-02-29 12:23:02 +0100

    Update pyhton_efficiency_description.py

--- pyhton_efficiency_description.py
+++ pyhton_efficiency_description.py
@@ -1,5 +1,5 @@
 Grpah avaviable here:
 https://docs.google.com/spreadsheets/d/1hCbbQUnJMUNjlAfgu0Yob3uiaOlp2zV20V9EdgaUmhs/edit?usp=sharing
-Funkcija sastāv no 1 iekšējā cikla un 4 sarbībām ar funkcijas mainīgo, viena no šīm darbībām ir kāpināšana, reziznāšana , dalīšanas un pieskiatīšana.
+Funkcija sastāv no 1 iekšējā cikla un 4 darbībām ar funkcijas mainīgo, viena no šīm darbībām ir kāpināšana, reziznāšana , dalīšanas un pieskiatīšana.
 Ņemot vērā ka funkcija ir kāpināšanas darbība, kā arī pārējās un mainīgā vērtība pieaug, izmantotās atmiņas un laika grafiki būsn eksponensiāli piaugoši.
 
