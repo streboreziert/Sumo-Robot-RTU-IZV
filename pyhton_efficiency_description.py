548c50a96c896af9deaa7e638de0c06dbf6b45df
Roberts Treize <93783050+streboreziert@users.noreply.github.com>
2024-02-29 12:25:09 +0100

    Update pyhton_efficiency_description.py

--- pyhton_efficiency_description.py
+++ pyhton_efficiency_description.py
@@ -2,4 +2,4 @@ Grpah avaviable here:
 https://docs.google.com/spreadsheets/d/1hCbbQUnJMUNjlAfgu0Yob3uiaOlp2zV20V9EdgaUmhs/edit?usp=sharing
 Funkcija sastāv no 1 iekšējā cikla un 4 darbībām ar funkcijas mainīgo, viena no šīm darbībām ir kāpināšana, reziznāšana , dalīšanas un pieskiatīšana.
 Ņemot vērā ka funkcija ir kāpināšanas darbība, kā arī pārējās un mainīgā vērtība pieaug, izmantotās atmiņas un laika grafiki būsn eksponensiāli piaugoši.
-
+Tas ir viena iemesla dēļ, ka binārajā sistēmā lielāki skaitļi un darbības ar lielākiem skaitļiem aizņem lielāku atmiņas apjomu, kā arī to aprēķiāšana aizņem vairāk laika.
