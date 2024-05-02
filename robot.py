13f5419d6e8ae9edad5dbf1a70542369e5f1a139
Roberts Treize <93783050+streboreziert@users.noreply.github.com>
2024-05-02 13:07:47 +0300

    Update robot.py

--- robot.py
+++ robot.py
@@ -802,5 +802,96 @@ class VL53L0X():
 
         else:
             self.motor[mId][0].duty_u16(0xFFFF)
-            self.motor[mId][1].duty_u16(0xF
+            self.motor[mId][1].duty_u16(0xF)
+import vl53l0x
+from machine import Pin, PWM, ADC, I2C
+import neopixel
+import time
+import robot
+from imu import Imu
+from button import Button
+
+np = neopixel.NeoPixel(Pin(14), 6)
+
+btn = Button(27)
+
+ir = PWM(Pin(5), freq=38000, duty_u16=0x8000)
+senL = Pin(35, Pin.IN, Pin.PULL_UP)
+senFL = Pin(34, Pin.IN, Pin.PULL_UP)
+senR = Pin(4, Pin.IN, Pin.PULL_UP)
+senFR = Pin(23, Pin.IN, Pin.PULL_UP)
+
+buzzer = PWM(Pin(25), duty_u16=0)
+
+i2c = I2C(0, sda=Pin(32), scl=Pin(33), freq=400000)
+i2c1 = I2C(1, sda=Pin(21), scl=Pin(22), freq=400000)
+
+flrL = ADC(Pin(36))
+flrR = ADC(Pin(39))
+flrL.atten(ADC.ATTN_11DB)
+flrR.atten(ADC.ATTN_11DB)
+
+i2c = I2C(0, sda=Pin(32), scl=Pin(33), freq=400000)
+tof = vl53l0x.VL53L0X(i2c)
+tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
+tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
+
+rob = robot.Robot(i2c1)
+
+flr = ADC(Pin(36))
+flr.atten(ADC.ATTN_11DB)
+
+a=0
+b=0
+c=0
+d=0
+colorvalue=0
+
+def neosen(which,numb):
+    
+     if which==0:
+        np[numb]=(200,0,0)
+        np.write()
+     if which==1:
+        np[numb]=(0,0,0)
+        np.write()
+        
+while True:
+    l=1
+    fl=senFL.value()
+    r=senR.value()
+    fr=senFR.value()
+    print()
+    event = btn.getEvent()
+    neosen(l,1)
+    neosen(fl,2)
+    neosen(r,4)
+    neosen(fr,3)
+    time.sleep(0.00001)
+    if(event == Button.PRESS):
+      colorvalue=flr.read()
+      print(colorvalue)
+    elif(event == Button.RELEASE):
+      colorvalue2=flr.read()
+    if  flr.read()>colorvalue:
+        if r==1 and l==1 :
+                rob.stop()
+                rob.drive(20000,0)       
+        if r==0:
+            rob.drive(20000,0)
+        if l==0:
+            rob.drive(0,20000)
+        if fl==0 and fr==1:
+            rob.drive(0,20000)
+        if fr==0 and fl==1:
+             rob.drive(20000,0)
+        if fl==0 and fr==0:
+            rob.drive(20000,20000)
+    else:
+        rob.drive(0,0)
+   
+   
+          
+          
+          
     
